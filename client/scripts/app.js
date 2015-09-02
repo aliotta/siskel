1fcb997fd8d0b42bf95d1130542088eb53dd6a3e
Alex Liotta <aliotta343@gmail.com>
2015-09-02 11:57:22 -0700

    Passed All Specs

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,9 +3,14 @@ var Movie = Backbone.Model.extend({
   defaults: {
     like: true
   },
+  
 
   toggleLike: function() {
-    // your code here
+    if(this.get('like') === true){
+      this.set('like',false)
+    } else {
+      this.set('like',true)
+    }
   }
 
 });
@@ -15,13 +20,14 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
+    this.on("change", function(){this.sort()})
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    this.comparator = field;
+    this.sort();
   }
 
 });
@@ -58,7 +64,7 @@ var MovieView = Backbone.View.extend({
                         </div>'),
 
   initialize: function() {
-    // your code here
+    this.model.on("change", this.render,this);
   },
 
   events: {
@@ -66,7 +72,7 @@ var MovieView = Backbone.View.extend({
   },
 
   handleClick: function() {
-    // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -79,7 +85,7 @@ var MovieView = Backbone.View.extend({
 var MoviesView = Backbone.View.extend({
 
   initialize: function() {
-    // your code here
+    this.collection.on("sort", this.render, this)
   },
 
   render: function() {
